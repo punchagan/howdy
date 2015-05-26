71a069ce00a08f9fab82c8fd988abf9847156100
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-05-26 13:45:16 +0530

    Rename library to howdy.

--- howdy.el
+++ howdy.el
@@ -1,53 +1,48 @@
-;;; keepintouch.el --- A library to help you keep in touch with people.
+;;; howdy.el --- A library to help you keep in touch with people.
 
 ;; Copyright (C) 2015 Puneeth Chaganti
 ;; Author: Puneeth Chaganti <punchagan@muse-amuse.in>
 ;; Created: 24 May 2015
 ;; Keywords: contacts, org-mode, org-contacts
-;; Homepage: http://github.com/punchagan/keepintouch
+;; Homepage: http://github.com/punchagan/howdy
 
 ;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
 
-;; Keep in touch is a library to help you keep in touch with people.
+;; Howdy! is a library to help you keep in touch with people.
 
-;; The library currently supports using org-contacts to maintain your contacts
-;; database.
+;; The library currently only supports using org-contacts to maintain your
+;; contacts database.
 
 ;; You can configure the frequency at which you wish to contact a person by
-;; adding a `KEEPINTOUCH_INTERVAL' property to your contacts.
+;; adding a `HOWDY_INTERVAL' property to your contacts.
 
-;; You can add information about when you last contacted a person, by using the
-;; `keepintouch' function.  This adds a LOGNOTE to your contacts db entry for
-;; the person, with the current timestamp.
+;; You can update information about when you last contacted a person, by using
+;; the `howdy-contacted' function.  For example, (howdy-contacted "John Doe").
 
-;; The `keepintouch-contacts' function can be used to add agenda entries for
-;; contacts who are out of touch.
-;; Adding an entry like:
+;; To get agenda entries for out-of-touch contacts, use `howdy-howdy'. For
+;; example, add an entry like the one below to one of your org-contacts-files.
 ;;
 ;; * Keep in touch
-;; %%(keepintouch-contacts)
-;;
-;; to an org-contacts-file will add agenda entries with list of people who are
-;; out of touch.
+;; %%(howdy-contacts)
 
 (require 'org-contacts)
 
-(defgroup keepintouch nil
-  "Options for keepintouch.")
+(defgroup howdy nil
+  "Options for howdy.")
 
-(defcustom keepintouch-interval-property "KEEPINTOUCH_INTERVAL"
-  "Name of the property for keepintouch interval."
+(defcustom howdy-interval-property "HOWDY_INTERVAL"
+  "Name of the property for howdy interval."
   :type 'string
-  :group 'keepintouch)
+  :group 'howdy)
 
-(defcustom keepintouch-last-contacted-property "KEEPINTOUCH_LAST_CONTACTED"
+(defcustom howdy-last-contacted-property "LAST_HOWDY"
   "Name of the property for last contacted timestamp."
   :type 'string
-  :group 'keepintouch)
+  :group 'howdy)
 
-(defcustom keepintouch-agenda-entry-format "Say Howdy: %l (%p) (%e)"
+(defcustom howdy-agenda-entry-format "Say Howdy: %l (%p) (%e)"
   "Format of the \"say howdy!\" agenda entry.
 The following replacements are available:
 
@@ -56,9 +51,9 @@ The following replacements are available:
   %p - Phone number
   %e - Email"
   :type 'string
-  :group 'keepintouch)
+  :group 'howdy)
 
-(defun keepintouch (name &optional time)
+(defun howdy-contacted (name &optional time)
   "Update last contacted time for the contact.
 
 If TIME is nil, `org-log-note-effective-time' is used."
@@ -73,10 +68,10 @@ If TIME is nil, `org-log-note-effective-time' is used."
                           (org-time-stamp-format 'long t) time)))
           (switch-to-buffer-other-window (marker-buffer marker))
           (goto-char marker)
-          (org-set-property keepintouch-last-contacted-property timestamp))
+          (org-set-property howdy-last-contacted-property timestamp))
       (error (format "No contact %s found!" name)))))
 
-(defun keepintouch-contacts (&optional format)
+(defun howdy-howdy (&optional format)
   "Returns agenda entries for out-of-touch contacts.
 
 Format is a string matching the following format specification:
@@ -86,29 +81,29 @@ Format is a string matching the following format specification:
   %p - Phone number
   %e - Email"
   (let ()
-    (unless format (setq format keepintouch-agenda-entry-format))
-    (loop for contact in (keepintouch-backlog-contacts)
+    (unless format (setq format howdy-agenda-entry-format))
+    (loop for contact in (howdy--backlog-contacts)
           collect (format-spec format
                                `((?l . ,(org-with-point-at (cadr contact) (org-store-link nil)))
                                  (?h . ,(car contact))
                                  (?p . ,(cdr (assoc-string org-contacts-tel-property (caddr contact))))
                                  (?e . ,(cdr (assoc-string org-contacts-email-property (caddr contact)))))))))
 
-(defun keepintouch-backlog-contacts ()
+(defun howdy--backlog-contacts ()
   "Returns a list of contacts who need to be contacted."
   (loop for contact in (org-contacts-db)
-	  if (keepintouch-backlog-contact-p contact)
+	  if (howdy--backlog-contact-p contact)
 	  collect contact))
 
-(defun keepintouch-backlog-contact-p (contact)
+(defun howdy--backlog-contact-p (contact)
   (let ((interval
          (ignore-errors
            (string-to-number
-            (cdr (assoc-string keepintouch-interval-property (caddr contact))))))
+            (cdr (assoc-string howdy-interval-property (caddr contact))))))
         (last-contacted
          (ignore-errors
            (org-parse-time-string
-            (cdr (assoc-string keepintouch-last-contacted-property (caddr contact)))))))
+            (cdr (assoc-string howdy-last-contacted-property (caddr contact)))))))
     (when interval
       (if last-contacted
           (< interval
@@ -116,6 +111,6 @@ Format is a string matching the following format specification:
               (time-since (apply 'encode-time last-contacted))))
         t))))
 
-(provide 'keepintouch)
+(provide 'howdy)
 
-;;; keepintouch.el ends here
+;;; howdy.el ends here
