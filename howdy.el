aa922f7ce85695a981f2befe2ed3871a8ae85949
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-05-26 16:08:33 +0530

    Add GPLv3 License.

--- howdy.el
+++ howdy.el
@@ -6,8 +6,22 @@
 ;; Keywords: contacts, org-mode, org-contacts
 ;; Homepage: http://github.com/punchagan/howdy
 
-;; This file is not part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Commentary:
 
 ;; Howdy! is a library to help you keep in touch with people.
